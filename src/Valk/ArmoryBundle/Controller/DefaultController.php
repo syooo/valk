ff73b668f404cc5ecab13425bf8f8e786af42cf2
syooo <syooo@mail.ru>
2014-01-30 03:19:21 +0400

    Rename Default to Static

--- src/Valk/ArmoryBundle/Controller/DefaultController.php
+++ src/Valk/ArmoryBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('ValkArmoryBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ValkArmoryBundle:Static:index.html.twig', array('name' => $name));
     }
 }
--- src/Valk/WowBundle/Controller/DefaultController.php
+++ src/Valk/WowBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('ValkWowBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ValkWowBundle:Static:index.html.twig', array('name' => $name));
     }
 }
